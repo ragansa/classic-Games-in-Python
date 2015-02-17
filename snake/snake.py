f68bf378ca2557a0fa6aa6d6a6655478cf7aea62
Scott <ragansa@gmail.com>
2015-02-16 17:18:37 -0800

    changed image access to a dictionary

--- snake/snake.py
+++ snake/snake.py
@@ -13,51 +13,78 @@ snakeHead_image = pyglet.image.load('snake_heads.png')
 snakeTail_image = pyglet.image.load('snake_tails.png')
 snakeSeg_image = pyglet.image.load('snake_segs.png')
 apple_image = pyglet.image.load('apple.png')
-centerAnchor(apple_image)
 segments = pyglet.image.ImageGrid(snakeSeg_image, 3, 2).get_texture_sequence()
 heads = pyglet.image.ImageGrid(snakeHead_image, 2, 2).get_texture_sequence()
 tails = pyglet.image.ImageGrid(snakeTail_image, 2, 2).get_texture_sequence()
 
-snake_segments = [segment for segment in segments]
-snake_segments += [head for head in heads]
-snake_segments += [tail for tail in tails]
+segmentsDict = {}
+segmentsDict['DownRight'] = segments[0]
+segmentsDict['LeftUp'] = segments[0]
+segmentsDict['DownLeft'] = segments[1]
+segmentsDict['RightUp'] = segments[1]
+segmentsDict['UpRight'] = segments[2]
+segmentsDict['LeftDown'] = segments[2]
+segmentsDict['UpLeft'] = segments[3]
+segmentsDict['RightDown'] = segments[3]
+segmentsDict['Up'] = segments[4]
+segmentsDict['Down'] = segments[4]
+segmentsDict['Left'] = segments[5]
+segmentsDict['Right'] = segments[5]
+segmentsDict['HeadLeft'] = heads[0]
+segmentsDict['HeadDown'] = heads[1]
+segmentsDict['HeadUp'] = heads[2]
+segmentsDict['HeadRight'] = heads[3]
+segmentsDict['TailLeft'] = tails[0]
+segmentsDict['TailDown'] = tails[1]
+segmentsDict['TailUp'] = tails[2]
+segmentsDict['TailRight'] = tails[3]
 
-for segment in snake_segments:
-    centerAnchor(segment)
-    
+for segment in segmentsDict:
+    centerAnchor(segmentsDict[segment])
+centerAnchor(apple_image)   
 
+#-------------------------------------------------------------------------------
 class World():
-
+    appleImage = apple_image
+    
     def __init__(self, snake):
         self.snake = snake
         self.score = 0
         self.apple = self.findAppleXY()
+        self.keyPressed = False
+      
         
     def draw(self):
         self.snake.draw()
-        apple_image.blit(self.apple.x, self.apple.y)
+        self.appleImage.blit(self.apple.x, self.apple.y)
         pyglet.text.Label(str(self.score), font_size=14, x= 30, y = 20).draw()
         
+        
     def findAppleXY(self):
-        newX = random.randrange(0, WINDOW_X, 20)
-        newY = random.randrange(0, WINDOW_Y, 20)
-        apple = Apple(newX, newY)
-        if self.snake.detectCollision(apple):
+        newX = random.randrange(20, WINDOW_X, 20)
+        newY = random.randrange(20, WINDOW_Y, 20)
+        newApple = Apple(newX, newY)
+        if self.snake.detectCollision(newApple):
             return self.findAppleXY()
-        return apple
+        return newApple
+    
     
     def keyPress(self, symbol):
-        currentDir = self.snake.direction
-        if symbol == pyglet.window.key.UP and currentDir != "Down":
-            self.snake.direction = "Up"
-        if symbol == pyglet.window.key.RIGHT and currentDir != "Left":
-            self.snake.direction = "Right"
-        if symbol == pyglet.window.key.LEFT and currentDir != "Right":
-            self.snake.direction = "Left"
-        if symbol == pyglet.window.key.DOWN and currentDir != "Up":
-            self.snake.direction = "Down"
+        if not self.keyPressed:
+            self.keyPressed = True
+            currentDir = self.snake.direction
+            if symbol == pyglet.window.key.UP and currentDir != "Down":
+                self.snake.direction = "Up"
+            if symbol == pyglet.window.key.RIGHT and currentDir != "Left":
+                self.snake.direction = "Right"
+            if symbol == pyglet.window.key.LEFT and currentDir != "Right":
+                self.snake.direction = "Left"
+            if symbol == pyglet.window.key.DOWN and currentDir != "Up":
+                self.snake.direction = "Down"
+    
     
     def onTick(self):
+        self.keyPressed = False
         if self.snake.onTick():
             self.snake = Snake(WINDOW_X // 2, 480 // 2, 5)
             self.apple = self.findAppleXY()
@@ -67,24 +94,9 @@ class World():
             self.apple = self.findAppleXY()
             self.score += self.snake.length
         
-
+#-------------------------------------------------------------------------------
 class Snake():
-    image = snake_segments
-    # image[0] - bend:top-right
-    # image[1] - bend:top-left
-    # image[2] - bend:bottom-right
-    # image[3] - bend:bottom-left
-    # image[4] - body: up-down
-    # image[5] - body: left-right
-    # image[6] - head: left
-    # image[7] - head: down
-    # image[8] - head: up
-    # image[9] - head: right
-    # image[10] - tail: left
-    # image[11] - tail: down
-    # image[12] - tail: right
-    # image[13] - tail: up
-    
+    imageDict = segmentsDict
     #directions:
         # "Up"
         # "Right"
@@ -93,12 +105,14 @@ class Snake():
     
     def __init__(self, x, y, length):
         self.direction = "Up"
-        self.head = SnakeSegment(x, y, self.direction, self.image[8])
+        self.head = SnakeSegment(x, y, self.direction, self.imageDict["Up"])
         self.length = length
+        
         
     def addSegment(self, segment):
         segment.next = self.head
         self.head = segment
+        
         
     def removeTail(self):
         count = self.length
@@ -108,36 +122,33 @@ class Snake():
             curr = curr.next
             count -= 1
         if lastDir == "Left":
-            newTail = self.image[13]
+            newTail = self.imageDict["TailRight"]
         elif lastDir == "Up":
-            newTail = self.image[11]
+            newTail = self.imageDict["TailDown"]
         elif lastDir == "Right":
-            newTail = self.image[10]
+            newTail = self.imageDict["TailLeft"]
         else:
-            newTail = self.image[12]
+            newTail = self.imageDict["TailUp"]
         curr.image = newTail
         curr.next = None
+        
         
     def onTick(self):
         if self.direction == "Up":
             delta_x = 0
             delta_y = 20
-            newImage = self.image[8]
         if self.direction == "Right":
             delta_x = 20
             delta_y = 0
-            newImage = self.image[9]
-            self.head.image = self.image[4]
         if self.direction == "Down":
             delta_x = 0
             delta_y = -20
-            newImage = self.image[7]
-            self.head.image = self.image[4]
         if self.direction == "Left":
             delta_x = -20
             delta_y = 0
-            newImage = self.image[6]
-            self.head.image = self.image[4]
+
+        self.head.image = self.imageDict[self.direction]
+        newImage = self.imageDict["Head" + self.direction]
             
         self.head.image = self.newNeckImage(self.head.direction, self.direction)
         self.addSegment(SnakeSegment(self.head.x + delta_x, 
@@ -149,35 +160,12 @@ class Snake():
             return True
         return self.head.x > WINDOW_X + 40 or self.head.x < 0 - 40 or self.head.y > WINDOW_Y + 40 or self.head.y < 0 - 40
     
+    
     def newNeckImage(self, oldDir, newDir):
-        if oldDir == "Up":
-            if newDir == "Right":
-                return self.image[2]
-            if newDir == "Left":
-                return self.image[3]
-            else:
-                return self.image[4]
-        if oldDir == "Down":
-            if newDir == "Right":
-                return self.image[0]
-            if newDir == "Left":
-                return self.image[1]
-            else:
-                return self.image[4]
-        if oldDir == "Right":
-            if newDir == "Up":
-                return self.image[1]
-            if newDir == "Down":
-                return self.image[3]
-            else:
-                return self.image[5]
-        if oldDir == "Left":
-            if newDir == "Up":
-                return self.image[0]
-            if newDir == "Down":
-                return self.image[2]
-            else:
-                return self.image[5]
+        if oldDir == newDir:
+            newDir = ""
+        return self.imageDict[oldDir + newDir]
+        
         
     def detectCollision(self, item):
         current = self.head
@@ -189,8 +177,10 @@ class Snake():
             current = current.next
         return False
         
+        
     def collision(self, item1, item2):
         return item1.x == item2.x and item1.y == item2.y
+        
         
     def draw(self):
         current = self.head
@@ -198,6 +188,7 @@ class Snake():
             current.draw()
             current = current.next
 
+#-------------------------------------------------------------------------------
 class SnakeSegment():
     
     def __init__(self, x, y, direction, image):
@@ -209,13 +200,15 @@ class SnakeSegment():
 
     def draw(self):
         self.image.blit(self.x, self.y)
-        
+
+#-------------------------------------------------------------------------------        
 class Apple():
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+#-------------------------------------------------------------------------------
 
 window = pyglet.window.Window(WINDOW_X, 480)
 snake = Snake(WINDOW_X // 2, 480 // 2, 5)
